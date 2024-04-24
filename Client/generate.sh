a4f17dd609de64e7270929d5a1397fd23d3baee9
Äkwav <ekwav@coflnet.com>
2024-04-24 11:17:30 +0200

    target .net 6 for client

--- Client/generate.sh
+++ Client/generate.sh
@@ -1,14 +1,17 @@
-VERSION=0.2.2
+VERSION=0.2.3
 
 docker run --rm -v "${PWD}:/local" --network host -u $(id -u ${USER}):$(id -g ${USER})  openapitools/openapi-generator-cli generate \
 -i http://localhost:5009/swagger/v1/swagger.json \
 -g csharp \
--o /local/out --additional-properties=packageName=Coflnet.Sky.Crafts.Client,packageVersion=$VERSION,licenseId=MIT
+-o /local/out --additional-properties=packageName=Coflnet.Sky.Crafts.Client,packageVersion=$VERSION,licenseId=MIT,targetFramework=net6.0
 
 cd out
-sed -i 's/GIT_USER_ID/Coflnet/g' src/Coflnet.Sky.Crafts.Client/Coflnet.Sky.Crafts.Client.csproj
-sed -i 's/GIT_REPO_ID/SkyCrafts/g' src/Coflnet.Sky.Crafts.Client/Coflnet.Sky.Crafts.Client.csproj
-sed -i 's/>OpenAPI/>Coflnet/g' src/Coflnet.Sky.Crafts.Client/Coflnet.Sky.Crafts.Client.csproj
+path=src/Coflnet.Sky.Crafts.Client/Coflnet.Sky.Crafts.Client.csproj
+sed -i 's/GIT_USER_ID/Coflnet/g' $path
+sed -i 's/GIT_REPO_ID/SkyCrafts/g' $path
+sed -i 's/>OpenAPI/>Coflnet/g' $path
+sed -i 's@annotations</Nullable>@annotations</Nullable>\n    <PackageReadmeFile>README.md</PackageReadmeFile>@g' $path
+sed -i 's@Remove="System.Web" />@Remove="System.Web" />\n    <None Include="../../../../README.md" Pack="true" PackagePath="\"/>@g' $path
 
 dotnet pack
-cp src/Coflnet.Sky.Crafts.Client/bin/Debug/Coflnet.Sky.Crafts.Client.*.nupkg ..
+cp src/Coflnet.Sky.Crafts.Client/bin/Release/Coflnet.Sky.Crafts.Client.*.nupkg ..
