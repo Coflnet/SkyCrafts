b51dd00fe647cccf10bdfce545d121f2f8f668d7
Äkwav <ekwav@coflnet.com>
2024-07-07 11:21:06 +0200

    make Main syncronous

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Coflnet.Sky.Crafts
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
