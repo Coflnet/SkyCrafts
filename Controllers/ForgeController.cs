02dfeefe6cf3bacd9c8043e648ea3b24caca240d
Äkwav <ekwav@coflnet.com>
2024-07-14 22:25:33 +0200

    Add forge controller

---
+++ Controllers/ForgeController.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+using Coflnet.Sky.Crafts.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Coflnet.Sky.Crafts.Controllers;
+[ApiController]
+[Route("[controller]")]
+[ResponseCache(Duration = 180, Location = ResponseCacheLocation.Any, NoStore = false)]
+public class ForgeController : ControllerBase
+{
+    private readonly ILogger<ForgeController> _logger;
+    private ForgeCraftService forgeService;
+
+    public ForgeController(ILogger<ForgeController> logger, ForgeCraftService forgeService)
+    {
+        _logger = logger;
+        this.forgeService = forgeService;
+    }
+
+    [HttpGet]
+    [Route("all")]
+    [ResponseCache(Duration = 60, Location = ResponseCacheLocation.Any, NoStore = false)]
+    public IEnumerable<ForgeFlip> GetAll()
+    {
+        return forgeService.FlipList.OrderByDescending(f => f.CraftData.Profit);
+    }
+}
