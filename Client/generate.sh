03ab220d66b9c1bad5ff81400ef6463932178cf7
Äkwav <ekwav@coflnet.com>
2024-11-06 10:59:14 +0100

    update client and packages

--- Client/generate.sh
+++ Client/generate.sh
@@ -1,17 +1,19 @@
-VERSION=0.4.0
+VERSION=0.4.1
+PACKAGE_NAME=Coflnet.Sky.Crafts.Client
 
 docker run --rm -v "${PWD}:/local" --network host -u $(id -u ${USER}):$(id -g ${USER})  openapitools/openapi-generator-cli generate \
 -i http://localhost:5009/swagger/v1/swagger.json \
 -g csharp \
--o /local/out --additional-properties=packageName=Coflnet.Sky.Crafts.Client,packageVersion=$VERSION,licenseId=MIT,targetFramework=net6.0
+-o /local/out --additional-properties=packageName=$PACKAGE_NAME,packageVersion=$VERSION,licenseId=MIT,targetFramework=net6.0
 
 cd out
-path=src/Coflnet.Sky.Crafts.Client/Coflnet.Sky.Crafts.Client.csproj
+path=src/$PACKAGE_NAME/$PACKAGE_NAME.csproj
 sed -i 's/GIT_USER_ID/Coflnet/g' $path
 sed -i 's/GIT_REPO_ID/SkyCrafts/g' $path
 sed -i 's/>OpenAPI/>Coflnet/g' $path
 sed -i 's@annotations</Nullable>@annotations</Nullable>\n    <PackageReadmeFile>README.md</PackageReadmeFile>@g' $path
-sed -i 's@Remove="System.Web" />@Remove="System.Web" />\n    <None Include="../../../../README.md" Pack="true" PackagePath="\"/>@g' $path
+sed -i '34i    <None Include="../../../../README.md" Pack="true" PackagePath="\"/>' $path
 
 dotnet pack
-cp src/Coflnet.Sky.Crafts.Client/bin/Release/Coflnet.Sky.Crafts.Client.*.nupkg ..
+cp src/$PACKAGE_NAME/bin/Release/$PACKAGE_NAME.*.nupkg ..
+dotnet nuget push ../$PACKAGE_NAME.$VERSION.nupkg --api-key $NUGET_API_KEY --source "nuget.org" --skip-duplicate
