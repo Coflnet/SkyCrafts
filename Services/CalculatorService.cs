06e377028044f8775d6c0275c143223f31fe8b95
Äkwav <ekwav@coflnet.com>
2025-01-31 22:33:01 +0100

    reduce multistep craft cost

--- Services/CalculatorService.cs
+++ Services/CalculatorService.cs
@@ -41,7 +41,7 @@ namespace Coflnet.Sky.Crafts.Services
                     var canBeCrafteDirectly = CanBeCraftedDirectly(lookup, item);
                     if (crafts.TryGetValue(item.ItemId, out ProfitableCraft craft))
                     {
-                        var craftWithProfit = craft.CraftCost * item.Count * 1.12;
+                        var craftWithProfit = craft.CraftCost * item.Count * 1.05;
                         item.CraftCost = craft.CraftCost * item.Count;
                         if (!canBeCrafteDirectly)
                             craftWithProfit *= 100;
