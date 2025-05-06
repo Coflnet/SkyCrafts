2db8bc025d9a0546531b5494270a6cca38e1a8d7
Äkwav <ekwav@coflnet.com>
2025-05-06 10:49:04 +0200

    simplify crafting recipe service

--- Services/CraftingReceipeService.cs
+++ Services/CraftingReceipeService.cs
@@ -6,94 +6,92 @@ using Coflnet.Sky.Crafts.Models;
 using System;
 using System.Linq;
 
-namespace Coflnet.Sky.Crafts.Services
+namespace Coflnet.Sky.Crafts.Services;
+public class CraftingRecipeService
 {
-    public class CraftingRecipeService
+    public IEnumerable<ItemData> CraftAbleItems()
     {
-        public IEnumerable<ItemData> CraftAbleItems()
+        foreach (var itemPath in Directory.EnumerateFiles("itemData/items"))
         {
-            foreach (var itemPath in Directory.EnumerateFiles("itemData/items"))
+            ItemData item = null;
+            try
             {
-                ItemData item = null;
-                try
+                item = JsonSerializer.Deserialize<ItemData>(File.ReadAllText(itemPath));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error on: " + itemPath);
+                Console.WriteLine(e);
+            }
+            // item needs to have a dedicated recipe
+            if (item?.recipe != null || item?.recipes != null && item.recipes.Count > 0 && (item.recipes[0].type == "forge" || item.recipes[0].type == "crafting"))
+                yield return item;
+            if (itemPath.Contains("_NPC.json"))
+            {
+                var npc = JsonSerializer.Deserialize<NPC>(File.ReadAllText(itemPath));
+                if (npc.recipes == null)
                 {
-                    item = JsonSerializer.Deserialize<ItemData>(File.ReadAllText(itemPath));
+                    continue;
                 }
-                catch (System.Exception e)
+                foreach (var recipe in npc.recipes)
                 {
-                    Console.WriteLine("Error on: " + itemPath);
-                    Console.WriteLine(e);
-                }
-                // item needs to have a dedicated recipe
-                if (item?.recipe != null || item?.recipes != null && item.recipes.Count > 0 && (item.recipes[0].type == "forge" || item.recipes[0].type == "crafting"))
-                    yield return item;
-                if (itemPath.Contains("_NPC.json"))
-                {
-                    var npc = JsonSerializer.Deserialize<NPC>(File.ReadAllText(itemPath));
-                    if (npc.recipes == null)
-                    {
-                        continue;
-                    }
-                    foreach (var recipe in npc.recipes)
-                    {
-                        yield return PortToItem(recipe);
-                    }
+                    yield return PortToItem(recipe);
                 }
             }
-            var carpentry = JsonSerializer.Deserialize<Dictionary<string, List<Ingredient>>>(File.ReadAllText("Data/carpentry.json"));
-            foreach (var recipe in carpentry)
+        }
+        var carpentry = JsonSerializer.Deserialize<Dictionary<string, List<Ingredient>>>(File.ReadAllText("Data/carpentry.json"));
+        foreach (var recipe in carpentry)
+        {
+            yield return new ItemData()
             {
-                yield return new ItemData()
-                {
-                    itemid = recipe.Key,
-                    internalname = recipe.Key,
-                    displayname = recipe.Key,
-                    Type = "carpentry",
-                    recipes = new List<NewRecipe>()
-                    {
+                itemid = recipe.Key,
+                internalname = recipe.Key,
+                displayname = recipe.Key,
+                Type = "carpentry",
+                recipes =
+                    [
                         new NewRecipe()
                         {
                             type = "carpentry",
                             inputs = recipe.Value.ConvertAll(i => i.ItemId.Replace(":","-") + ":" + i.Count),
                             result = recipe.Key
                         }
-                    }
-                };
-            }
-        }
-
-        private static ItemData PortToItem(NPCRecipe recipe)
-        {
-            return new ItemData()
-            {
-                itemid = recipe.result,
-                internalname = recipe.result,
-                displayname = recipe.result,
-                Type = "npc",
-                recipes = new List<NewRecipe>()
-                            {
-                                new NewRecipe()
-                                {
-                                    type = recipe.type,
-                                    inputs = recipe.cost,
-                                    result = recipe.result
-                                }
-                            }
+                    ]
             };
         }
+    }
 
-        public async Task<Recipe> GetRecipe(string id)
+    private static ItemData PortToItem(NPCRecipe recipe)
+    {
+        return new ItemData()
         {
-            var itemData = await GetItemData(id);
-            return itemData?.recipe ?? itemData?.recipes?.FirstOrDefault();
-        }
+            itemid = recipe.result,
+            internalname = recipe.result,
+            displayname = recipe.result,
+            Type = "npc",
+            recipes =
+                [
+                    new NewRecipe()
+                    {
+                        type = recipe.type,
+                        inputs = recipe.cost,
+                        result = recipe.result
+                    }
+                ]
+        };
+    }
 
-        public async Task<ItemData> GetItemData(string id)
-        {
-            var path = $"itemData/items/{id}.json";
-            if (!File.Exists(path))
-                return null;
-            return JsonSerializer.Deserialize<ItemData>(await File.ReadAllTextAsync(path));
-        }
+    public async Task<Recipe> GetRecipe(string id)
+    {
+        var itemData = await GetItemData(id);
+        return itemData?.recipe ?? itemData?.recipes?.FirstOrDefault();
+    }
+
+    public async Task<ItemData> GetItemData(string id)
+    {
+        var path = $"itemData/items/{id}.json";
+        if (!File.Exists(path))
+            return null;
+        return JsonSerializer.Deserialize<ItemData>(await File.ReadAllTextAsync(path));
     }
 }
