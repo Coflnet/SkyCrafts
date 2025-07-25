0f54ecc8c203582a31f4dfa8668d2847e84e96a3
Äkwav <ekwav@coflnet.com>
2025-07-25 22:01:53 +0200

    update minion calculation

--- Controllers/DropController.cs
+++ Controllers/DropController.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using Coflnet.Sky.Crafts.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace Coflnet.Sky.Crafts.Controllers;
 
@@ -32,15 +34,18 @@ public class DropController : ControllerBase
     [HttpGet]
     [Route("minions")]
     [ResponseCache(Duration = 10, Location = ResponseCacheLocation.Any, NoStore = false)]
-    public IEnumerable<MinionRate> GetMinionDrops([FromServices] CraftingRecipeService craftingRecipeService)
+    public IEnumerable<MinionRate> GetMinionDrops()
     {
-        var minions = craftingRecipeService.CraftAbleItems().Where(c => c.internalname.Contains("_GENERATOR_")).ToList();
+        var minions = GetMinions().ToList();
         return minions.Select(m =>
         {
             var kind = m.internalname.Split("_GENERATOR_")[0];
-            var seconds = m.lore?.First(l => l.StartsWith("§7Time Between Actions: §a")).Replace("§7Time Between Actions: §a", "").Replace("s", "") ?? "0";
-            var maxStorage = m.lore?.First(l => l.StartsWith("§7Max Storage: §e")).Replace("§7Max Storage: §e", "").Replace(" coins", "") ?? "0";
+            var seconds = m.lore?.FirstOrDefault(l => l.StartsWith("§7Time Between Actions: §a"))?.Replace("§7Time Between Actions: §a", "").Replace("s", "") ?? "0";
+            if(seconds == "0")
+                Console.WriteLine($"No time found for {m.internalname} in {JsonConvert.SerializeObject(m)}");
+            var maxStorage = m.lore?.FirstOrDefault(l => l.StartsWith("§7Max Storage: §e"))?.Replace("§7Max Storage: §e", "").Replace(" coins", "") ?? "0";
             var tier = int.Parse(m.internalname.Split("_GENERATOR_")[1]);
+            kind = m.displayname.Substring(2, m.displayname.Length - 2 - m.displayname.Split(' ').Last().Length).Trim();
             return (kind, tier, seconds, maxStorage);
         }).GroupBy(m => m.kind)
         .Select(g => new MinionRate()
@@ -50,6 +55,17 @@ public class DropController : ControllerBase
             Capacity = g.OrderBy(m => m.tier).Select(m => int.Parse(m.maxStorage)).ToArray()
         }).OrderBy(m => m.Name);
     }
+
+    private IEnumerable<Models.ItemData> GetMinions()
+    {
+        foreach (var itemPath in Directory.EnumerateFiles("itemData/items"))
+        {
+            if(!itemPath.Contains("_GENERATOR_"))
+                continue;
+            yield return JsonConvert.DeserializeObject<Models.ItemData>(System.IO.File.ReadAllText(itemPath));
+        }
+    }
+
 
     public class MinionRate
     {
