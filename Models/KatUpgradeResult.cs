b2543f6894b059f5a7cf953b31a9161b90b71d2c
Äkwav <ekwav@coflnet.com>
2025-09-10 12:12:47 +0200

    make katupgrade properties public

--- Models/KatUpgradeResult.cs
+++ Models/KatUpgradeResult.cs
@@ -7,9 +7,9 @@ namespace Coflnet.Sky.Crafts.Models
     {
         public KatUpgradeCost CoreData { get; set; }
         public string OriginAuction { get; set; }
-        public double MaterialCost { get; internal set; }
-        public double UpgradeCost { get; internal set; }
-        public double Profit { get; internal set; }
+        public double MaterialCost { get; set; }
+        public double UpgradeCost { get; set; }
+        public double Profit { get; set; }
         public Api.Client.Model.Tier TargetRarity { get; set; }
         public string ReferenceAuction { get; set; }
         public long PurchaseCost { get; set; }
