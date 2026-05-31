67e599c10477cf100de88c67b3b8e22500fd4254
Ekwav <ekwav@users.noreply.github.com>
2026-05-31 11:35:47 +0200

    add openbao support

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Coflnet.Security.OpenBao;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
@@ -13,6 +14,7 @@ namespace Coflnet.Sky.Crafts
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((_, config) => config.AddOpenBaoFromEnvironment())
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
